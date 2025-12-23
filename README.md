318dfed409db4c9d84f560b66e19af9430430e24
Your Name <your.email@example.com>
2025-12-23 19:17:42 +0300

    Демонстрационный проект

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 
 ### SonarQube:
 [![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=Kamran92_frontend-project-44&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=Kamran92_frontend-project-44)
+
+### brain-even.js:
+[![asciicast](https://asciinema.org/a/fwPOeDO1b8ghNJ6aFGCaSGvgf.svg)](https://asciinema.org/a/fwPOeDO1b8ghNJ6aFGCaSGvgf)
--- bin/src/cli.js
+++ bin/src/cli.js
@@ -3,7 +3,38 @@ import readlineSync from 'readline-sync'
 export const greetUser = () => {
   console.log('Welcome to the Brain Games!')
 
-  const userName = readlineSync.question('May I have your name?')
+  const userName = readlineSync.question('May I have your name? ')
 
   console.log(`Hello, ${userName}!`)
+}
+
+export const playEvenGame = () => {
+  console.log('Welcome to the Brain Games!')
+
+  const userName = readlineSync.question('May I have your name? ')
+
+  console.log(`Hello, ${userName}!`)
+  console.log('Answer "yes" if the number is even, otherwise answer "no".')
+
+  const roundsCount = 3
+
+  for (let i = 0; i < roundsCount; i += 1) {
+    const randomNumber = Math.floor(Math.random() * 100) + 1
+    const correctAnswer = randomNumber % 2 === 0 ? 'yes' : 'no'
+
+    console.log(`Question: ${randomNumber}`)
+
+    const userAnswer = readlineSync.question('Your answer: ').toLowerCase()
+
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
+      console.log(`Let's try again, ${userName}!`)
+
+      return
+    }
+
+    console.log('Correct!')
+  }
+
+  console.log(`Congratulations, ${userName}!`)
 }
